0dbcab24a99ccda42ce67064639665d55be57997
Ryan Barrault <barraultryan@hotmail.fr>
2023-02-05 17:42:14 +0100

    nomJoueurs SAFE2 v050223

--- src/vue/EcranCreationPartie.java
+++ src/vue/EcranCreationPartie.java
@@ -13,7 +13,9 @@ public class EcranCreationPartie extends JPanel implements ActionListener, ItemL
     private JButton boutonRetourAccueil;
     private int nbJoueurs; // prévoir getter ?
     private JComboBox jcbNbJoueurs, jcbModeJeu;
-    private JTextField barreJ1, barreJ2, barreJ3, barreJ4, barreJ5;
+    private JLabel labelJ1;
+    private JTextField barreJ2, barreJ3, barreJ4, barreJ5;
+    private JButton boutonSuivant;
 
     public EcranCreationPartie() {
         this.setBackground(Color.lightGray);
@@ -43,10 +45,7 @@ public class EcranCreationPartie extends JPanel implements ActionListener, ItemL
                 setEcranCrea_Accueil();
                 break;
 
-            case 2: // nom Joueurs
-                setEcranCrea_NomJoueurs();
-                break;
-            case 3: // choix de ses cartes
+            case 2: // choix de ses cartes
                 setEcranCrea_ChoixMesCartes();
                 break;
         }
@@ -55,7 +54,7 @@ public class EcranCreationPartie extends JPanel implements ActionListener, ItemL
     // -----------------CASE1------------------
     public void setEcranCrea_Accueil() {
 
-        String l[] = { "---x---", "3", "4", "5" };
+        String l[] = { "--- x ---", "3", "4", "5" };
         String l2[] = { "Classique (Questions au joueur de gauche)", "Expert (Question au joueur de son choix)" };
 
         JLabel texteNbJoueurs = new JLabel("Quel est le nombre de joueurs de cette partie ?");
@@ -70,6 +69,12 @@ public class EcranCreationPartie extends JPanel implements ActionListener, ItemL
         jcbNbJoueurs.setBounds(420, 50, 80, 30);
         jcbModeJeu.setBounds(280, 100, 280, 30);
 
+        // Permet d'aligner le texte au centre du JComboBox
+        DefaultListCellRenderer centr = new DefaultListCellRenderer();
+        centr.setHorizontalAlignment(JLabel.CENTER);
+        jcbNbJoueurs.setRenderer(centr);
+        jcbModeJeu.setRenderer(centr);
+
         this.add(texteNbJoueurs);
         this.add(texteModeJeu);
         this.add(jcbNbJoueurs);
@@ -77,20 +82,16 @@ public class EcranCreationPartie extends JPanel implements ActionListener, ItemL
     }
 
     // -----------------CASE2------------------
-    public void setEcranCrea_NomJoueurs() {
-
-    }
-
-    // -----------------CASE3------------------
     public void setEcranCrea_ChoixMesCartes() {
-
+        cleanEcran();
+        afficherBoutonRetour();
     }
     // ----------------------------------------
 
     @Override
     public void itemStateChanged(ItemEvent e) {
         if (e.getSource() == jcbNbJoueurs) { // si on change le nb de joueurs
-            if (jcbNbJoueurs.getSelectedItem() == "---x---") {
+            if (jcbNbJoueurs.getSelectedItem() == "--- x ---") {
                 JOptionPane.showMessageDialog(this, "Vous devez choisir un nombre de Joueurs !");
             } else {
                 try {
@@ -101,88 +102,56 @@ public class EcranCreationPartie extends JPanel implements ActionListener, ItemL
                     afficherBoutonRetour();
                     setEcranCrea_Accueil();
 
-                    if (nbJoueurs == 3) {
-                        JLabel lab1 = new JLabel("Joueur1");
-                        JLabel lab2 = new JLabel("Joueur2");
-                        JLabel lab3 = new JLabel("Joueur3");
-                        lab1.setBounds(120, 180, 60, 30);
-                        lab2.setBounds(120, 230, 60, 30);
-                        lab3.setBounds(120, 280, 60, 30);
-                        barreJ1 = new JTextField();
-                        barreJ2 = new JTextField();
-                        barreJ3 = new JTextField();
-                        barreJ1.setBounds(180, 180, 300, 30);
-                        barreJ2.setBounds(180, 230, 300, 30);
-                        barreJ3.setBounds(180, 280, 300, 30);
-                        this.add(lab1);
-                        this.add(lab2);
-                        this.add(lab3);
-                        this.add(barreJ1);
-                        this.add(barreJ2);
-                        this.add(barreJ3);
+                    JLabel lab1 = new JLabel("Joueur1");
+                    JLabel lab2 = new JLabel("Joueur2");
+                    JLabel lab3 = new JLabel("Joueur3");
+                    lab1.setBounds(120, 180, 60, 30);
+                    lab2.setBounds(120, 230, 60, 30);
+                    lab3.setBounds(120, 280, 60, 30);
+                    labelJ1 = new JLabel("Moi");
+                    Font pol = new Font("Serif", Font.BOLD, 20);
+                    labelJ1.setFont(pol);
+                    barreJ2 = new JTextField();
+                    barreJ3 = new JTextField();
+                    labelJ1.setBounds(180, 180, 300, 30);
+                    barreJ2.setBounds(180, 230, 300, 30);
+                    barreJ3.setBounds(180, 280, 300, 30);
+                    labelJ1.setHorizontalAlignment(SwingConstants.CENTER);
+                    barreJ2.setHorizontalAlignment(SwingConstants.CENTER);
+                    barreJ3.setHorizontalAlignment(SwingConstants.CENTER);
+                    boutonSuivant = new JButton("Suivant");
+                    boutonSuivant.setBounds(440, 500, 120, 50);
+                    boutonSuivant.addActionListener(this);
+                    this.add(lab1);
+                    this.add(lab2);
+                    this.add(lab3);
+                    this.add(labelJ1);
+                    this.add(barreJ2);
+                    this.add(barreJ3);
+                    this.add(boutonSuivant);
 
-                    } else if (nbJoueurs == 4) {
-                        JLabel lab1 = new JLabel("Joueur1");
-                        JLabel lab2 = new JLabel("Joueur2");
-                        JLabel lab3 = new JLabel("Joueur3");
+                    if (nbJoueurs == 4) {
                         JLabel lab4 = new JLabel("Joueur4");
-
-                        lab1.setBounds(120, 180, 60, 30);
-                        lab2.setBounds(120, 230, 60, 30);
-                        lab3.setBounds(120, 280, 60, 30);
                         lab4.setBounds(120, 330, 60, 30);
-
-                        barreJ1 = new JTextField();
-                        barreJ2 = new JTextField();
-                        barreJ3 = new JTextField();
                         barreJ4 = new JTextField();
-
-                        barreJ1.setBounds(180, 180, 300, 30);
-                        barreJ2.setBounds(180, 230, 300, 30);
-                        barreJ3.setBounds(180, 280, 300, 30);
                         barreJ4.setBounds(180, 330, 300, 30);
-
-                        this.add(lab1);
-                        this.add(lab2);
-                        this.add(lab3);
+                        barreJ4.setHorizontalAlignment(SwingConstants.CENTER);
                         this.add(lab4);
-                        this.add(barreJ1);
-                        this.add(barreJ2);
-                        this.add(barreJ3);
                         this.add(barreJ4);
 
                     } else if (nbJoueurs == 5) {
-                        JLabel lab1 = new JLabel("Joueur1");
-                        JLabel lab2 = new JLabel("Joueur2");
-                        JLabel lab3 = new JLabel("Joueur3");
                         JLabel lab4 = new JLabel("Joueur4");
                         JLabel lab5 = new JLabel("Joueur5");
-
-                        lab1.setBounds(120, 180, 60, 30);
-                        lab2.setBounds(120, 230, 60, 30);
-                        lab3.setBounds(120, 280, 60, 30);
                         lab4.setBounds(120, 330, 60, 30);
                         lab5.setBounds(120, 380, 60, 30);
-
-                        barreJ1 = new JTextField();
-                        barreJ2 = new JTextField();
-                        barreJ3 = new JTextField();
                         barreJ4 = new JTextField();
                         barreJ5 = new JTextField();
-
-                        barreJ1.setBounds(180, 180, 300, 30);
-                        barreJ2.setBounds(180, 230, 300, 30);
-                        barreJ3.setBounds(180, 280, 300, 30);
                         barreJ4.setBounds(180, 330, 300, 30);
                         barreJ5.setBounds(180, 380, 300, 30);
-                        this.add(lab1);
-                        this.add(lab2);
-                        this.add(lab3);
+                        barreJ4.setHorizontalAlignment(SwingConstants.CENTER);
+                        barreJ5.setHorizontalAlignment(SwingConstants.CENTER);
                         this.add(lab4);
                         this.add(lab5);
-                        this.add(barreJ1);
-                        this.add(barreJ2);
-                        this.add(barreJ3);
                         this.add(barreJ4);
                         this.add(barreJ5);
                     }
@@ -198,6 +167,28 @@ public class EcranCreationPartie extends JPanel implements ActionListener, ItemL
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == boutonRetourAccueil) {
             FenetreAppli.choixEcranAffiche(1);
+        } else if (e.getSource() == boutonSuivant) {
+            if (!barreJ2.getText().equals("") &&
+                    (!barreJ3.getText().equals(""))) {
+                if (nbJoueurs == 3) {
+                    setEcranCrea_ChoixMesCartes();
+                } else if (nbJoueurs == 4) {
+                    if (!barreJ4.getText().equals("")) {
+                        setEcranCrea_ChoixMesCartes();
+                    } else {
+                        JOptionPane.showMessageDialog(this, "Vous devez entrer le nom de tous les joueurs !");
+                    }
+                } else if (nbJoueurs == 5) {
+                    if ((!barreJ4.getText().equals("")) &&
+                            (!barreJ5.getText().equals(""))) {
+                        setEcranCrea_ChoixMesCartes();
+                    } else {
+                        JOptionPane.showMessageDialog(this, "Vous devez entrer le nom de tous les joueurs !");
+                    }
+                }
+            } else {
+                JOptionPane.showMessageDialog(this, "Vous devez entrer le nom de tous les joueurs !");
+            }
         }
     }
 }
